5c124525f46b619f9a5b2bb0598a4b1884850bd1
S4AK4N <kd1378732@st.kobedenshi.ac.jp>
2024-06-20 12:53:36 +0900

    3

--- Scc.c
+++ Scc.c
@@ -1,51 +1,141 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+// トークンの種類
+typedef enum {
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
 
-int main(int argc, char **argv)
-{
-	//もし引数が2つじゃなかったら
-	if(argc != 2)
-	{
-		fprintf(stderr,"引数の個数が正しくありません\n");
-		return 1;
-	}
+typedef struct Token Token;
 
-	char *p = argv[1]; //argv[1]のアドレスをポインタ変数pに格納
-	
-	//出力結果をリダイレクト時にアセンブリにできる用
-	printf(".intel_syntax noprefix\n");
- 	printf(".globl main\n");
-        printf("main:\n");
-	printf("  mov rax, %ld\n", strtol(p, &p, 10)); //atoiでは文字列をint型に直してくれるだけであり文字数は返してくれないので次の項をどれを読めばいいかわからなくなる。そのためstrtolを使用
-						       
-//strtolは数値を読み込んだあとポインタをアップデートする。つまりアドレス内の値が数値だったとき整数にして返すが
-//文字列(今回だと + , -)だったときはアドレスのみが加算されて返ってくるのでそのアドレス内の値と- or + が一致すると加算する仕組みを作れば良い
+// トークン型
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+};
 
-	//*p回ループ
-	while(*p)
-	{
-		//もしpアドレスの中身が+と一致するとき
-		if(*p == '+')
-		{
-			p++;//pアドレスを加算(つまり次のアドレスを参照して)
-			printf("  add rax, %ld\n", strtol(p, &p, 10)); //値を見に行く
-      			continue;
-		}
-		
-		//もしpアドレスの中身が-と一致するとき
-		if (*p == '-') 
-		{
-      			p++;
-      			printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      			continue;
-   		}
+// 現在着目しているトークン
+Token *token;
 
-    		fprintf(stderr, "予期しない文字です: '%c'\n", *p);
-    		return 1;
+// エラーを報告するための関数
+// printfと同じ引数を取る
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
 
-	}
+// 次のトークンが期待している記号のときには、トークンを1つ読み進めて
+// 真を返す。それ以外の場合には偽を返す。
+bool consume(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    return false;
+  token = token->next;
+  return true;
+}
 
-	  printf("  ret\n");
-  	  return 0;
+// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
+// それ以外の場合にはエラーを報告する。
+void expect(char op) {
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error("'%c'ではありません", op);
+  token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
+// それ以外の場合にはエラーを報告する。
+int expect_number() {
+  if (token->kind != TK_NUM)
+    error("数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  cur->next = tok;
+  return tok;
+}
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    error("引数の個数が正しくありません");
+    return 1;
+  }
+
+  // トークナイズする
+  token = tokenize(argv[1]);
+
+  // アセンブリの前半部分を出力
+  printf(".intel_syntax noprefix\n");
+  printf(".globl main\n");
+  printf("main:\n");
+
+  // 式の最初は数でなければならないので、それをチェックして
+  // 最初のmov命令を出力
+  printf("  mov rax, %d\n", expect_number());
+
+  // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
+  // アセンブリを出力
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_number());
+      continue;
+    }
+
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
+  }
+
+  printf("  ret\n");
+  return 0;
 }
